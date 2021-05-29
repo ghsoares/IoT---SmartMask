c98b9da9b5b99853af9dd87537af744bee2cf9f1
GHSoares <ghsoares99795@gmail.com>
2021-05-28 21:30:42 -0300

    .

--- SmartMask.ino
+++ SmartMask.ino
@@ -15,12 +15,24 @@ int pinLed = 5;
 Pino do sensor de temperatura baseado em resistência
 */
 int pinTherm = A0;
+/*
+Pino do sensor de umidade (resistência)
+*/
+int pinUmidade = A1;
 // Distância mínima detectada em centímetros
 int distanceMin = 2;		
 // Distância máxima em centímetros
 int distanceMax = 30;		
 
+// Intensidade máxima do led
 int maxIntensity = 125;
+
+// Resistência do termistor
+float tempR = 10000;
+// Temperatura atual
+float temp;
+// umidade relativa atual
+float umid;
 
 void setup() {
 	// Inicia o serial
@@ -33,14 +45,23 @@ void loop() {
 	// Distância atual em centímetros
 	int distMao = sensorDistancia.distance();
 	
-	// Temperatura atual
-	int temp = analogRead(pinTherm);
+	// Lê a temperatura atual
+	temp = readTemp(pinTherm, tempR);
+	
+	// Lê a umidade relativa atual
+	umid = (float)analogRead(pinUmidade);
+	
+	// Mapeia a umidade relativa (0-1023 para 0%-100%)
+	umid = map(umid, 0, 1023, 0, 100);
 	
 	// [Debug] Mostra no monitor serial a distância atual
 	Serial.println(String("Distância: ") + distMao + String(" cm"));
 	
 	// [Debug] Mostra no monitor serial a temperatura atual
 	Serial.println(String("Temperatura: ") + temp + String(""));
+	
+	// [Debug] Mostra no monitor serial a umidade relativa atual
+	Serial.println(String("Umidade: ") + umid + String("%"));
 	
 	// Limita a distância para evitar overflow do método map
 	distMao = constrain(distMao, distanceMin, distanceMax);
@@ -60,6 +81,26 @@ void loop() {
 	*/
 	analogWrite(pinLed, ledIntensity);
 	
+	// Envia os dados dos sensores para o aplicativo (react-native) dados como temperatura, umidade, etc.
+	// TODO: Enviar dados do sensor para o aplicativo
+	
 	// Espera um décimo de segundo
-	delay(100);
+	delay(1000);
+}
+
+//função que faz leitura da temperatura e retorna o valor em graus celcius
+float readTemp(int pin, float resistencia){
+	//valores constante para calculo da temperatura
+	float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07; 
+
+	// Lê a entrada
+  float tempOut = (float)analogRead(pin);
+//calculo R2, demonstração no arquivo pdf da aula
+  tempOut = resistencia * (1023.0 / tempOut - 1.0);
+  tempOut = log(tempOut);
+// Equação de Steinhart–Hart 
+  tempOut = (1.0 / (c1 + c2*tempOut + c3*tempOut*tempOut*tempOut));// Equação de Steinhart–Hart 
+  tempOut = tempOut - 273.15; //temperatura em Graus celcius
+  //Tf = (Tc * 9.0)/ 5.0 + 32.0; // temp em fahrenheit
+  return tempOut;
 }
